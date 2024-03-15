19ad230668ffc923df0ec70d7dcbabab7cf4fc08
Lucas.Delvoie <lucasdelvoie@gmail.com>
2024-03-15 15:11:07 -0400

    1

---
+++ assignment_4/part_1/part_2/main.js
@@ -0,0 +1,26 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+
+/* Declaring the alternative text for each image file */
+const alttexts = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic4.jpg', 'pic5.jpg'];
+/* Looping through images */
+
+for (let i = 0; i < 6; i++) {
+
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+    newImage.setAttribute('alt', alttexts[i]);
+    thumbBar.appendChild(newImage);
+}
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
