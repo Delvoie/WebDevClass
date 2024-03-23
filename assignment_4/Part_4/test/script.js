f45b370e3b53d68c5d3eb97a09c716691657c5a9
Lucas.Delvoie <lucasdelvoie@gmail.com>
2024-03-22 21:10:35 -0400

    test

--- assignment_4/Part_4/test/script.js
+++
@@ -1,8 +0,0 @@
-const startButton = document.querySelector('.start-button');
-const content = document.querySelector('.content');
-
-startButton.addEventListener('click', () => {
-  startButton.style.display = 'none';
-  document.body.style.backgroundColor = 'white';
-  content.style.display = 'block';
-});
