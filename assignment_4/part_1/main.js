1f4f09ef34b81234a766764c6bf5cccabcada931
Lucas.Delvoie <lucasdelvoie@gmail.com>
2024-04-01 14:59:42 -0400

    sfdsf

--- assignment_4/part_1/main.js
+++ assignment_4/part_1/main.js
@@ -1,6 +1,5 @@
 // File: main.js
 // Name: Lucas Delvie
-// Date: 17 April  2024
 // A very brief description of the file.
 // -->
 
@@ -13,14 +12,14 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-//step 2
+//step One
 storytext = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 
  insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
  insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
  insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
- //step 3
+ //step TWO
  randomize.addEventListener('click', result);
 
  function result() {
@@ -40,7 +39,6 @@ storytext = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When th
     newstory = newstory.replace('94 fahrenheit', temperature).replace('300 pounds', weight);
   }
 
-  // Replace placeholders with random values first
   newstory = newstory.replace(/:insertx/g, xItem).replace(/:inserty/g, yItem).replace(/:insertz/g, zItem);
 
   story.textContent = newstory;
