8a12aa45034c64a60cd2769f2ad5e48b3769cc1b
Lucas.Delvoie <lucasdelvoie@gmail.com>
2024-09-09 19:59:06 -0400

    asda

--- Helloworld2.java
+++ Helloworld2.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         byte x = 6;
-        byte y=x;
+        byte y=8;
         long y =-123_456_789L;
     }
 }
