a815e683529a51c08eb8c045b57560b9d5c852b3
Lucas.Delvoie <lucasdelvoie@gmail.com>
2024-09-12 10:57:42 -0400

    etstasasd

--- autiomation/assigment1.py
+++ autiomation/assigment1.py
@@ -13,27 +13,36 @@
 #area = 3.14 * radius**2
 #print (area)
 
-a= 9
-a=10
-print(a)
+#a= 9
+#a=10
+#print(a)
 
-hourly_wage = input("Please enter your hourly wage: ")
-print("Hourly wage: ")
-print(hourly_wage)
+#hourly_wage = input("Please enter your hourly wage: ")
+#print("Hourly wage: ")
+#print(hourly_wage)
 
-hours_worked= input("Hours worked: ")
-print(hours_worked)
-hours_worked = input("How many hours did you work this week? ")
+#hours_worked= input("Hours worked: ")
+#print(hours_worked)
+#hours_worked = input("How many hours did you work this week? ")
 
-salary = hourly_wage* hours_worked
+#salary = hourly_wage* hours_worked
 
-print("Your salary is: ", salary)
+#print("Your salary is: ", salary)
 
 
 #part 2
 
 
-a= "{name} is {age} years old, and {name} works as a {jpb}."
-print(a.format(name="John", age=32, jpb="Engineer"))
+#a= "{name} is {age} years old, and {name} works as a {jpb}."
+#print(a.format(name="John", age=32, jpb="Engineer"))
 
 
+#name = "lucas"
+#a=name.title()
+#age= 21
+#print(f"{a} is {age} years old")
+
+print("Hello, World!".lower()) # "hello, world!"
+print("Hello, World!".upper()) # "HELLO, WORLD!"
+print("Hello, World!".capitalize()) # "Hello, world!"
+print("hello, world!".title()) # "Hello, World!"
