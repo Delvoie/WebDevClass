f7813b8d4b7fb33d2942e4376345aee42ab05d99
Lucas.Delvoie <lucasdelvoie@gmail.com>
2024-09-12 10:47:34 -0400

    asdasdS

--- autiomation/assigment1.py
+++ autiomation/assigment1.py
@@ -3,7 +3,19 @@
 # In class exercise 1
 
 #print("My age is 21")
-print (21*365)
-print (21*52)
-print (21*12)
+#print (21*365)
+#print (21*52)
+#print (21*12)
 
+#area of a circle
+
+#radius = 5
+#area = 3.14 * radius**2
+#print (area)
+
+
+
+
+#part 2
+output = "{} is {} years old, and {} works as a {}."
+print(output.format("John",24,"John","web developer"))
