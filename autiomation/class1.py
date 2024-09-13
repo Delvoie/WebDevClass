2df75c74ee1950666d4c6205650543771b3c5864
Lucas.Delvoie <lucasdelvoie@gmail.com>
2024-09-13 10:44:27 -0400

    df

--- autiomation/class1.py
+++ autiomation/class1.py
@@ -1,2 +1,19 @@
-for i in range(0,10,3):
-    print (i)
+employes = [
+    ("Rolf Smith", 35, 8.75),
+    ("Anne Pun", 30, 12.50),
+    ("Charlie Lee", 50, 15.50),
+    ("Bob Smith", 20, 7.00)
+]
+
+total = 0
+for emp in employes:
+    total += emp[2]  # Add the pay to the total
+
+avg = total / len(employes)  # Calculate the average pay
+
+print("Total Pay:", total)
+print("Average Pay:", avg)
+
+for emp in employes:
+    if emp[2] > avg:
+        print(f"{emp[0]} is more than average")
