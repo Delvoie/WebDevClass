c4f91bfdd0ef9deb5bed5eccb70440f0b753694b
Lucas.Delvoie <lucasdelvoie@gmail.com>
2024-09-13 11:45:50 -0400

    dsfsd

---
+++ autiomation/inclass1.py
@@ -0,0 +1,29 @@
+movies = [
+    ("Eternal Sunshine of the Spotless Mind", 20000000),
+    ("Memento", 9000000),
+    ("Requiem for a Dream", 4500000),
+    ("Pirates of the Caribbean: On Stranger Tides", 379000000),
+    ("Avengers: Age of Ultron", 365000000),
+    ("Avengers: Endgame", 356000000),
+    ("Incredibles 2", 200000000)
+]
+
+totalbudget = 0
+for mov in movies:
+    totalbudget += mov[1] 
+
+avg = totalbudget / len(movies)
+
+print("Total Budget:", totalbudget)
+print("Average Budget:", avg)
+
+for mov in movies:
+    if mov[1] > avg:  
+        Overbudget = mov[1] - avg
+        print(f"{mov[0]} has a budget greater than the average")
+        print(f"{mov[0]}'s budget is ${Overbudget:.2f} more than the average")
+overAvg = 0
+for mov in movies:
+    if mov[1] > avg:
+        overAvg += 1
+print(f"{overAvg} movies spent more than the average")
