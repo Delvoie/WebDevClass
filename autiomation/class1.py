f56663c72c92d72852ef9f65d42104a60e5fa5b9
Lucas.Delvoie <lucasdelvoie@gmail.com>
2024-09-13 11:29:54 -0400

    saddas

--- autiomation/class1.py
+++ autiomation/class1.py
@@ -29,9 +29,36 @@ sliced_string = original_string[0:2]
 print(sliced_string)"""
 
 
-
+'''''
 name = input("please enter your name").split()
 givenName=name[0] 
 lastName= name[1]
 print("given name is : ", givenName)
-print("lastName is: ", lastName)
+print("lastName is: ", lastName)'''
+
+'''''
+numbers = [1, 2, 3, 4, 5]
+num = []
+
+for n in numbers:
+    num.append(str(n))
+
+print(" | ".join(num))'''
+'''''
+user_number = input("Please enter a number: ")
+while float(user_number) < 10:
+ print("Your number was less than 10.")
+ user_number = input("Please select another number: ")
+print("Your number was at least 10.")
+'''
+
+
+
+totalbudget = 0
+for emp in Mmvies:
+    totalbudget += emp[2]  # Add the pay to the total
+
+avg = totalbudget / len(employes)  # Calculate the average pay
+
+print("Total Pay:", totalbudget)
+print("Average Pay:", avg)
