810ac77fb40665a9aca687322e3c97f854b635de
Lucas.Delvoie <lucasdelvoie@gmail.com>
2024-09-18 19:14:33 -0400

    sadad

--- autiomation/inclass1.py
+++ autiomation/inclass1.py
@@ -7,17 +7,23 @@ movies = [
     ("Avengers: Endgame", 356000000),
     ("Incredibles 2", 200000000)
 ]
+addmovie = int(input("How many movies do you wish to add? "))
+
+for mov in range(addmovie):
+    movie_name = input("Enter movie name: ")
+    movie_budget = float(input("Enter movie budget: "))
+    movies.append((movie_name, movie_budget))
 
 totalbudget = 0
 for mov in movies:
-    totalbudget += mov[1] 
+    totalbudget += mov[1]
 
 avg = totalbudget / len(movies)
 
 print("Average Budget:", avg)
 
 for mov in movies:
-    if mov[1] > avg:  
+    if mov[1] > avg:
         Overbudget = mov[1] - avg
         print(f"{mov[0]} has a budget greater than the average: ${Overbudget:.2f} more than the average")
 overAvg = 0
