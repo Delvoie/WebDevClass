247282c1c34fded3a0f363bfe271c72dfd291ad3
Lucas.Delvoie <lucasdelvoie@gmail.com>
2024-09-20 10:46:58 -0400

    ✨ feat:

--- autiomation/peopleSort.py
+++ autiomation/peopleSort.py
@@ -9,8 +9,19 @@ people.sort(key=lambda x:x['age'])
 
 print(people)"""""
 
-add= lambda x,y,z: x+y+z
+"""add= lambda x,y,z: x+y+z
 
 result = add(1,2,3)
 
-print(result)
+print(result)"""
+
+
+students = [ {"name": "Hannah", "grade_average": 83},
+ {"name": "Charlie", "grade_average": 91},
+ {"name": "Peter", "grade_average": 85},
+ {"name": "Rachel", "grade_average": 79},
+ {"name": "Lauren", "grade_average": 92} ]
+
+students.sort(key=lambda student: student["grade_average"])
+
+print(students)
