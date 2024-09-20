f3b66c37c68dc819f4181241d89d7b5ef7595d29
Lucas.Delvoie <lucasdelvoie@gmail.com>
2024-09-20 10:41:48 -0400

    asd

--- autiomation/peopleSort.py
+++ autiomation/peopleSort.py
@@ -1,7 +1,15 @@
-people= [{'name': 'bob', 'age': 20},
+"""people= [{'name': 'bob', 'age': 20},
         {'name': 'carry', 'age': 38},
         {'name': 'john', 'age': 7}
         
         ]
 people.sort(key=lambda x:x['age'])
-print(people)
+
+
+
+print(people)"""""
+
+add= lambda x,y,z: x+y+z
+
+print(result)
+
