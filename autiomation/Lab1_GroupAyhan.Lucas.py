82ad1bb12f206a40fced1edef8cc6a491ae7cbb1
Lucas.Delvoie <lucasdelvoie@gmail.com>
2024-09-24 21:28:29 -0400

    sada

--- autiomation/Lab1_GroupAyhan.Lucas.py
+++ autiomation/Lab1_GroupAyhan.Lucas.py
@@ -1,16 +1,50 @@
-length = input('Enter the length of the password: ')
-letters = input('Enter the number of letters desired in password: ')
-digits = input('Enter the numbers of digits desired in the password: ')
-special = input('Enter the numbers of special characters desired in the password: ')
+# Date: Sept 23, 2024
+# Name: "Password Generator"
+# Programmer: Ayhan Ahsan & Lucas Delvoie
+# Description: Generates a password with certain parameters
 
-def validate_password(length, letters, digits, special):
-    if length < 8:
-         print('Please enter value of greater or equal to 8 to create a secure password')
-    elif letters < 0 or letters > 9:
-         print('The number of letters should be in range of 0 and 9')
-    elif digits < 0 or digits > 6:
-         print('The number of Digits should be in range of 0 and 6')
-    elif special < 0 or special > 2:
-         print('The number of special characters should be in range of 0 and 2')
+# Import packages
+import random
+from string import ascii_letters, digits, punctuation
+
+# Function to get valid input 
+def valid_input(prompt, min_value, max_value):
+    while True:
+        try:
+            value = int(input(prompt))
+            if min_value <= value <= max_value:
+                return value
+            else:
+                print(f'Please enter a value between {min_value} and {max_value}')
+        except ValueError:
+            print("Please enter a numeric value.")
+
+# Password generator function k=random choices
+def generate_password(length, letters, num_digits, special):
+    password_chars = (
+        random.choices(ascii_letters, k=letters) +
+        random.choices(digits, k=num_digits) +
+        random.choices(punctuation, k=special) +
+        random.choices(ascii_letters + digits + punctuation, k=length - (letters + num_digits + special))
+    )
+    
+    # Joins letters, digits, and special characters
+    return ''.join(password_chars)
+
+def main_function():
+    length = valid_input('Enter the length of the password (minimum 8, max 128): ', 8, 128)
+    
+    # Makes sure that the sum of letters, digits, and special characters fit in parameters
+    letters = valid_input(f'Enter the number of letters (0-{length}): ', 0, length)
+    num_digits = valid_input(f'Enter the number of digits (0-{length - letters}): ', 0, length - letters)
+    special = valid_input(f'Enter the number of special characters (0-{length - letters - num_digits}): ', 0, length - letters - num_digits)
+    
+    # Check if the sum of letters, digits, and special characters can fit within the length
+    if length >= letters + num_digits + special:
+        password = generate_password(length, letters, num_digits, special)
+        print(f'Your generated password is: \n{password}')
     else:
-         print('Valid inputs')
+        print("Error: The sum of letters, digits, and special characters exceeds the total length.")
+
+if __name__ == "__main__":
+    main_function()
