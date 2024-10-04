7eb69959ed7378ecbed28d33e40a67e164a4c15e
Lucas.Delvoie <lucasdelvoie@gmail.com>
2024-10-04 10:59:46 -0400

    asda

---
+++ autiomation/past/addition.py
@@ -0,0 +1,12 @@
+class Addition(object):
+    def addition(self,a,b):
+        return a+b
+    
+
+
+
+    if __name__ == "__main__":
+        cal=Addition()
+        result=cal.addition(2,3)
+        print(result)
+        
